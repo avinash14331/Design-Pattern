d23688f01a7cbaf1fb02140abb39a989e91bfde3
Avinash <avinash14331@gmail.com>
2022-01-31 02:19:10 +0530

    prototype design pattern

---
+++ src/main/java/creational/prototype/Driver.java
@@ -0,0 +1,23 @@
+package creational.prototype;
+
+public class Driver {
+    public static void main(String[] args)
+    {
+        try
+        {
+            String moviePrototype  = PrototypeFactory.getInstance(PrototypeFactory.ModelType.MOVIE).toString();
+            System.out.println(moviePrototype);
+
+            String albumPrototype  = PrototypeFactory.getInstance(PrototypeFactory.ModelType.ALBUM).toString();
+            System.out.println(albumPrototype);
+
+            String showPrototype  = PrototypeFactory.getInstance(PrototypeFactory.ModelType.SHOW).toString();
+            System.out.println(showPrototype);
+
+        }
+        catch (CloneNotSupportedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
