d3b46fb485fb3542660d82d4d7a3188dd0b26238
Avinash <avinash14331@gmail.com>
2022-01-31 14:59:58 +0530

    Flyweight design pattern

---
+++ src/main/java/structural/flyweight/ThinPen.java
@@ -0,0 +1,14 @@
+package structural.flyweight;
+
+public class ThinPen implements Pen{
+    private final BrushSize brushSize = BrushSize.THIN;
+    private String color = null;
+    @Override
+    public void setColor(String color) {
+        this.color=color;
+    }
+    @Override
+    public void draw(String content) {
+        System.out.println("Drawing THIN content in color : " + color);
+    }
+}
